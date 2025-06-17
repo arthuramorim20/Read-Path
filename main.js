5ba0f16d08fb3ba351293fe781ab388653c24c95
Arthur Evaristo <aer47186@gmail.com>
2025-06-17 14:12:16 -0400

    ++

---
+++ main.js
@@ -0,0 +1,28 @@
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+class ReadText {
+    static getPath() {
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = dirname(__filename);
+        const pathFile = join(__dirname, "./mockText.txt");
+
+        return pathFile
+    }
+
+    static async readPath() {
+        const text = await readFile(this.getPath(), 'utf-8');
+
+        return text;
+    }
+}
+
+
+const reading = await ReadText.readPath();
+
+console.log(reading)
+
+// function textFile (filePath) {
+//     const
+// }
