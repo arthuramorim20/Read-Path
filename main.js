4fa4672f7025c2e79bf6b7646672f935f3587a27
Arthur <aer47186@gmail.com>
2025-06-18 09:17:32 -0400

    ++

--- main.js
+++ main.js
@@ -2,28 +2,46 @@ import { readFile } from 'fs/promises';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-class ReadText {
+class ReadCsv {
     static getPath() {
         const __filename = fileURLToPath(import.meta.url);
         const __dirname = dirname(__filename);
-        const pathFile = join(__dirname, "./mockText.txt");
+        const pathFile = join(__dirname, "./mockCSV.csv");
 
         return pathFile;
     }
 
     static async readPath() {
-        const text = await readFile(this.getPath(), 'utf-8');
+        const path = this.getPath();
+        const text = (await readFile(path)).toString('utf-8');
 
-        return text.split(',');
+        return text;
+    }
+
+    static async formatFile() {
+        const file = await this.readPath() 
+        const lines = file
+        .split('\n')
+        .map(Line => Line.trim())
+        .filter(Line => Line.length > 0);
+
+        const [ header, ...rows ] = lines;
+
+        const keys = header.split(',');
+        
+        const data = rows.map(row => {
+            const values = row.split(',');
+
+            return Object.fromEntries(keys.map((key, i) => [key, values[i]]));
+        })
+        
+        return data;    
     }
 }
 
 
-const reading = await ReadText.readPath();
+const reading = await ReadCsv.formatFile();
 
 console.log(reading)
 
 
-// function textFile (filePath) {
-//     const
-// }
